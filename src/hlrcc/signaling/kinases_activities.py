74121e4776fca196958daec522956ca46033be3d
Emanuel Goncalves <emanuelvgoncalves@gmail.com>
2016-02-09 18:09:58 +0000

    kinase activities pathway enrichment added; volcanos adapted

--- src/hlrcc/signaling/kinases_activities.py
+++ src/hlrcc/signaling/kinases_activities.py
@@ -5,17 +5,21 @@ import statsmodels.api as sm
 from hlrcc import wd
 from pymist.enrichment.gsea import gsea
 from pandas import DataFrame, Series, read_csv
+from statsmodels.stats.multitest import multipletests
+from pymist.utils.read_gmt import read_gmt
 from pymist.utils.omnipath_phospho import get_targets
+from pymist.utils.map_peptide_sequence import read_uniprot_genename
 
 
 # -- Kinase activity prediction function
-def calc_activity_lm(x, y):
+def calc_activity_lm(x, y, alpha=0.001):
     xs = x.ix[y.index].dropna()
+    xs = xs.loc[:, xs.sum() != 0]
     xs['Const'] = 1
 
     ys = y.ix[xs.index]
 
-    lm = sm.OLS(ys, xs).fit_regularized(L1_wt=0, alpha=0.001)
+    lm = sm.OLS(ys, xs).fit_regularized(L1_wt=0, alpha=alpha)
 
     return lm.params.drop('Const')
 
@@ -30,6 +34,11 @@ def calc_activity_gsea(x, y, permutations=10000):
     es = {k: -np.log10(es[k][1]) if es[k][0] < 0 else np.log10(es[k][1]) for k in es}
 
     return Series(es)
+
+
+# -- Import Uniprot id mapping
+human_uniprot = read_uniprot_genename()
+print '[INFO] Uniprot human protein: ', len(human_uniprot)
 
 
 # -- Import kinases targets
@@ -56,8 +65,6 @@ print '[INFO] Kinases activities estimated'
 # -- Plot kinases activities
 kinases_ov = list(set(k_activity_lm.index).intersection(k_activity_gsea.index))
 
-p_highlight = ['P00519']
-
 plot_df = DataFrame({'lm': k_activity_lm.ix[kinases_ov], 'gsea': k_activity_gsea.ix[kinases_ov]})
 
 sns.set(style='ticks')
@@ -68,8 +75,42 @@ g = sns.jointplot(
 plt.axhline(0, ls='--', lw=0.3, c='#95a5a6', alpha=.5)
 plt.axvline(0, ls='--', lw=0.3, c='#95a5a6', alpha=.5)
 g.plot_marginals(sns.kdeplot, shade=True, color='#34495e')
-# g.ax_joint.scatter(plot_df.ix[p_highlight, 'lm'], plot_df.ix[p_highlight, 'gsea'], marker='o', c='#34495e', lw=.5, edgecolor='#e74c3c')
 g.set_axis_labels('Kinase activities (Ridge)', 'Kinase activities (GSEA)')
 plt.savefig('%s/reports/kinases_activites_jointplot.pdf' % wd, bbox_inches='tight')
 plt.close('all')
 print '[INFO] Corr plotted!'
+
+
+# -- GSEA in MSigDB
+signatures = read_gmt('%s/files/c2.cp.kegg.v5.1.symbols.gmt' % wd)
+
+kinase_set = {human_uniprot[k][0]: v for k, v in k_activity_lm.to_dict().items() if k in human_uniprot}
+
+pathways_es = {p: gsea(kinase_set, signatures[p], 10000) for p in signatures}
+pathways_es = DataFrame(pathways_es, index=['es', 'pvalue']).T.dropna()
+pathways_es['fdr'] = multipletests(pathways_es['pvalue'], method='fdr_bh')[1]
+pathways_es['name'] = [' '.join(i.lower().split('_')[1:]) for i in pathways_es.index]
+pathways_es = pathways_es.sort('fdr', ascending=False)
+print pathways_es.tail(10).sort('fdr')
+
+plot_df = pathways_es[pathways_es['pvalue'] < .05]
+colours, y_pos = sns.color_palette('Paired', 2), [x + 1.5 for x in range(len(plot_df['name']))]
+
+plt.barh(y_pos, -np.log10(plot_df['pvalue']), lw=0, align='center', height=.5, color=colours[0], label='p-value')
+plt.barh(y_pos, -np.log10(plot_df['fdr']), lw=0, align='center', height=.5, color=colours[1], label='FDR')
+plt.yticks(y_pos, plot_df['name'])
+
+plt.axvline(-np.log10(0.05), ls='--', lw=0.4, c='gray')
+plt.axvline(-np.log10(0.01), ls='--', lw=0.4, c='gray')
+
+plt.text(-np.log10(0.05) * 1.01, .5, '5%', ha='left', color='gray', fontsize=9)
+plt.text(-np.log10(0.01) * 1.01, .5, '1%', ha='left', color='gray', fontsize=9)
+
+sns.despine()
+plt.xlabel('-log10')
+plt.title('KEGG pathways enrichment')
+plt.legend(loc=4)
+plt.savefig('%s/reports/k_activity_pathway_enrichment.pdf' % wd, bbox_inches='tight')
+plt.close()
+print '[INFO] Pathways enrichment plotted'
+
--- src/hlrcc/utils/volcano.py
+++ src/hlrcc/utils/volcano.py
@@ -13,38 +13,37 @@ def signif(value):
 
 
 def volcano(f, d, x_label, y_label, adj_p_value_label, title='', to_highlight=None, ids=None):
-    sns.set(style='white')
-    sns.set_context('paper')
-    sns.despine(offset=10)
+    # Define params
+    sns.set(style='ticks', context='paper')
 
     # Descritise significance
     d['signif'] = [signif(v) for v in d[adj_p_value_label]]
 
     # Define pallete
-    colour_pallete = [sns.color_palette('OrRd')[4], sns.color_palette('OrRd')[3], sns.color_palette('OrRd')[1]]
-    sns.lmplot(x=x_label, y=y_label, data=d, hue='signif', fit_reg=False, palette=colour_pallete, legend=False)
+    colour_pallete = sns.light_palette('#34495e', 4, reverse=True)[:-1]
+    g = sns.lmplot(x=y_label, y=x_label, data=d, hue='signif', fit_reg=False, palette=colour_pallete, legend=False)
+    g.axes[0,0].set_xlim(0,)
 
     # Add FDR threshold lines
-    plt.text(plt.xlim()[0]*.98, -np.log10(0.01), 'FDR 1%', ha='left', color=colour_pallete[0], alpha=0.65, fontsize=7)
-    plt.axhline(-np.log10(0.01), c=colour_pallete[0], ls='--', lw=.5, alpha=.7)
+    plt.text(-np.log10(0.01) * 1.01, plt.ylim()[0]*.98, 'FDR 1%', ha='left', color=colour_pallete[0], alpha=0.65, fontsize=5)
+    plt.axvline(-np.log10(0.01), c=colour_pallete[0], ls='--', lw=.7, alpha=.7)
 
-    plt.text(plt.xlim()[0]*.98, -np.log10(0.05), 'FDR 5%', ha='left', color=colour_pallete[1], alpha=0.65, fontsize=6)
-    plt.axhline(-np.log10(0.05), c=colour_pallete[1], ls='--', lw=.5, alpha=.7)
+    plt.text(-np.log10(0.05) * 1.01, plt.ylim()[0]*.98, 'FDR 5%', ha='left', color=colour_pallete[1], alpha=0.65, fontsize=5)
+    plt.axvline(-np.log10(0.05), c=colour_pallete[1], ls='--', lw=.7, alpha=.7)
 
     # Add axis lines
-    plt.axvline(0, c='#95a5a6', lw=.3, alpha=.15)
-    plt.axhline(0, c='#95a5a6', lw=.3, alpha=.15)
+    plt.axhline(0, c='#95a5a6', lw=.3, ls='-', alpha=.3)
 
     # Add axis labels and title
     plt.title(title, fontsize=10, fontname='Arial')
-    plt.xlabel('fold-change (log2)', fontsize=8, fontname='Arial')
-    plt.ylabel('p-value (-log10 FDR)', fontsize=8, fontname='Arial')
+    plt.ylabel('Fold-change (log2)', fontsize=8, fontname='Arial')
+    plt.xlabel('-log10 FDR ', fontsize=8, fontname='Arial')
 
     # Add text to highlighted genes
     if to_highlight is not None:
         for i, r in d.iterrows():
             if r[ids] in to_highlight:
-                plt.text(r[x_label] * 1.01, r[y_label] * 1.01, r[ids], ha='left', alpha=0.75, fontsize=8)
+                plt.text((r[y_label] * 1.01), (r[x_label] * 1.01), r[ids], ha='left', alpha=0.75, fontsize=8)
 
     # Adjust axis lines thickness
     ax = plt.gca()
@@ -54,8 +53,8 @@ def volcano(f, d, x_label, y_label, adj_p_value_label, title='', to_highlight=No
     ax.yaxis.set_tick_params(width=0.5)
 
     # Save plot
-    fig = plt.gcf()
-    fig.set_size_inches(5., 8.)
-    fig.savefig(f, bbox_inches='tight')
+    plt.gcf().set_size_inches(5., 3., forward=True)
+    # plt.xlim(0, plt.ylim()[1])
+    plt.savefig(f, bbox_inches='tight')
 
     print '[INFO] Volcano generated: ' + f
