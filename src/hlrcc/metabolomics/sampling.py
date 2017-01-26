41fac7799c38befa3f2d22c9cec2213623cec753
Emanuel Goncalves <emanuelvgoncalves@gmail.com>
2017-01-26 10:35:13 +0000

    metabolic sampling analysis updated

--- src/hlrcc/metabolomics/sampling.py
+++ src/hlrcc/metabolomics/sampling.py
@@ -2,15 +2,16 @@
 # Copyright (C) 2017 Emanuel Goncalves
 
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from pandas import read_csv, DataFrame, Series
-from framed import load_cbmodel, simplify, FBA, MOMA, pFBA, FVA, lMOMA
 from hlrcc.metabolomics.sampler import sample
+from framed import load_cbmodel, simplify, FBA, MOMA, pFBA, FVA, lMOMA, reaction_deletion, gene_deletion
 
 # -- Imports
 # CORE metabolomics (mmol/gDW/h)
 core = read_csv('./data/uok262_metabolomics_core_processed.csv', index_col=0).dropna().set_index('exchange')
-core = core[core['fdr'] < .05]
+core = core[core['fdr'] < .1]
 
 # Medium
 medium = read_csv('./files/DMEM_41966_medium.txt', sep='\t').dropna().set_index('exchange')
@@ -36,7 +37,7 @@ conditions = ['UOK262', 'UOK262pFH']
 
 res_fba, res_env = {}, {}
 
-# c = 'UOK262'
+# c = 'UOK262pFH'
 for c in conditions:
     print c
     res_fba[c] = {}
@@ -64,23 +65,78 @@ for c in conditions:
     res_env[c] = env
 
     # Biomass and ATP production
-    res_fba[c]['biomass'] = FBA(model, objective={'R_biomass_reaction': 1}, constraints=env).fobj
-    print res_fba[c]['biomass']
+    res_fba[c]['biomass'] = pFBA(model, objective={'R_biomass_reaction': 1}, constraints=env)
+    print res_fba[c]['biomass'].pre_solution.fobj
 
-    res_fba[c]['atp'] = FBA(model, objective={'R_ATPM': 1}, constraints=env, get_shadow_prices=True).fobj
-    print res_fba[c]['atp']
+    res_fba[c]['atp'] = pFBA(model, objective={'R_ATPM': 1}, constraints=env)
+    print res_fba[c]['atp'].pre_solution.fobj
 
-    # Sampler
-    sampling = sample(model, n_samples=100, n_steps=200, verbose=1, constraints=env)
-    sampling.to_csv('./data/%s_sampling.txt' % c, sep='\t', index=False)
-    print '[INFO] Sampling finished: ', c
+    # solution = res_fba[c]['atp']
+    # print solution.show_metabolite_balance('M_succoa_m', model)
+    # print solution.values['R_SUCOASm']
 
-# res_fba = DataFrame(res_fba).T
-#
-# print 'ATP yield / mol of Glucose: UOK262pFH %.2f, UOK262 %.2f' % (
-#     (res_fba.ix['UOK262pFH', 'atp'] / -res_env['UOK262pFH']['R_EX_glc_e']), (res_fba.ix['UOK262', 'atp'] / -res_env['UOK262']['R_EX_glc_e'])
-# )
-#
-# print 'Biomass yield / mol of Glucose: UOK262pFH %.2f, UOK262 %.2f' % (
-#     (res_fba.ix['UOK262pFH', 'biomass'] / -res_env['UOK262pFH']['R_EX_glc_e']), (res_fba.ix['UOK262', 'biomass'] / -res_env['UOK262']['R_EX_glc_e'])
-# )
+    # # Sampler
+    # env['R_ATPM'] = res_fba[c]['atp'].pre_solution.fobj
+    #
+    # sampling = sample(model, n_samples=200, n_steps=200, verbose=1, constraints=env)
+    # sampling.to_csv('./data/%s_sampling.txt' % c, sep='\t', index=False)
+    # print '[INFO] Sampling finished: ', c
+
+
+# -- Plotting
+pal = dict(zip(*(conditions, sns.light_palette('#34495e', 3, reverse=True).as_hex()[:-1])))
+
+# ATP and biomass yield
+plot_df = DataFrame([
+    {
+        'condition': 'UOK262pFH',
+        'atp': abs(res_fba['UOK262pFH']['atp'].pre_solution.fobj / res_env['UOK262pFH']['R_EX_glc_e']),
+        'biomass': abs(res_fba['UOK262pFH']['biomass'].pre_solution.fobj / res_env['UOK262pFH']['R_EX_glc_e'])
+    },
+    {
+        'condition': 'UOK262',
+        'atp': abs(res_fba['UOK262']['atp'].pre_solution.fobj / res_env['UOK262']['R_EX_glc_e']),
+        'biomass': abs(res_fba['UOK262']['biomass'].pre_solution.fobj / res_env['UOK262']['R_EX_glc_e'])
+    }
+])
+
+sns.set(style='ticks', context='paper', font_scale=.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3})
+sns.factorplot('condition', 'atp', data=plot_df, palette=pal, kind='bar', lw=0)
+plt.axhline(4, ls='--', lw=.3, c='gray')
+plt.axhline(32, ls='--', lw=.3, c='gray')
+plt.gcf().set_size_inches(1, 2)
+plt.ylabel('ATP yield (per mol Glucose)')
+plt.xlabel('')
+plt.savefig('./reports/atp_yield_bar.pdf', bbox_inches='tight')
+plt.close('all')
+print '[INFO] Plot done'
+
+sns.set(style='ticks', context='paper', font_scale=.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3})
+sns.factorplot('condition', 'biomass', data=plot_df, palette=pal, kind='bar', lw=0)
+plt.gcf().set_size_inches(1, 2)
+plt.ylabel('Biomass yield (per mol Glucose)')
+plt.xlabel('')
+plt.savefig('./reports/biomass_yield_bar.pdf', bbox_inches='tight')
+plt.close('all')
+print '[INFO] Plot done'
+
+# O2/CO2 rates
+plot_df = DataFrame([{'condition': c, 'o2': res_fba[c]['atp'].values['R_EX_o2_e'], 'co2': res_fba[c]['atp'].values['R_EX_co2_e']} for c in conditions])
+plot_df['o2'] *= -1
+
+sns.set(style='ticks', context='paper', font_scale=.75, rc={'axes.linewidth': .3, 'xtick.major.width': .3, 'ytick.major.width': .3})
+sns.regplot('co2', 'o2', plot_df[plot_df['condition'] == 'UOK262'], fit_reg=False, color=pal['UOK262'], label='UOK262')
+sns.regplot('co2', 'o2', plot_df[plot_df['condition'] == 'UOK262pFH'], fit_reg=False, color=pal['UOK262pFH'], label='UOK262pFH')
+sns.despine()
+plt.xlabel('CO2 secretion (mmol/gDW/h)')
+plt.ylabel('O2 consumption (mmol/gDW/h)')
+plt.legend(loc=3)
+plt.gcf().set_size_inches(2, 2)
+plt.savefig('./reports/ocr_ecar_scatter.pdf', bbox_inches='tight')
+plt.close('all')
+print '[INFO] Plot done'
+
+
+# -- Export simulations
+fluxes = DataFrame({c: res_fba[c]['atp'].values for c in res_fba})
+fluxes.to_csv('./data/pfba_atp.csv')
